f242e3abf31263e3cd67fd148d815d8a71f320c0
artem <artemqq5@gmail.com>
2024-03-28 15:45:51 +0200

    fix last

--- routers/request_join_.py
+++ routers/request_join_.py
@@ -13,12 +13,15 @@ async def handle_join_request(chat_join_request: types.ChatJoinRequest, bot: Bot
     link_from_db = LinkRepository().get_link(link)
 
     try:
-        if link_from_db and link_from_db['hello_message']:
-            await bot.send_message(chat_id=user_id, text=link_from_db['hello_message'])
+        if link_from_db:
             LinkRepository().update_link(user_join=link_from_db['users_join']+1, link=link)
 
-        await chat_join_request.approve()
+            if link_from_db['hello_message']:
+                await bot.send_message(chat_id=user_id, text=link_from_db['hello_message'])
+
     except Exception as e:
         print(f"handle_join_request: {e} | @{chat_join_request.from_user.username}")
+    finally:
+        await chat_join_request.approve()
 
 
