f67f731ad900ced2d72f87ff6dec79df9aa95ee9
artem <artemqq5@gmail.com>
2024-04-17 10:00:34 +0300

    add user to db when request send

--- routers/request_join_.py
+++ routers/request_join_.py
@@ -4,13 +4,16 @@ from aiogram.types import InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 from data.repositories.LinkRepository import LinkRepository
+from data.repositories.UserRepository import UserRepository
 
 router = Router()
 
 
 @router.chat_join_request()
 async def handle_join_request(chat_join_request: types.ChatJoinRequest, bot: Bot):
-    user_id = chat_join_request.from_user.id
+    user = chat_join_request.from_user
+    UserRepository().add_user(user.id, user.username, user.first_name, user.last_name)
+
     link = chat_join_request.invite_link.invite_link
 
     link_from_db = LinkRepository().get_link(link)
@@ -32,8 +35,10 @@ async def handle_join_request(chat_join_request: types.ChatJoinRequest, bot: Bot
                 [InlineKeyboardButton(text="Open", url=link_from_db['link'])]
             ])
 
-            await bot.send_message(chat_id=user_id,
+            await bot.send_message(chat_id=user.id,
                                    text=link_from_db['hello_message'],
                                    reply_markup=kb_join.as_markup())
     except Exception as e:
         print(f"handle_join_request: {e} | @{chat_join_request.from_user.username}")
+
+
